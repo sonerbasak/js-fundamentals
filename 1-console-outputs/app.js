4ddb40b6214b92f5ece52781977e0e2d923ccce9
Snrbsk <sonerbasaak@gmail.com>
2025-11-12 10:02:22 +0300

    learning output types

---
+++ 1-console-outputs/app.js
@@ -0,0 +1,23 @@
+//* using document.write() to output text directly to the HTML document
+
+document.write("document.write()");
+
+// * using console.log() to output text to the browser's console
+
+console.log("console.log()");
+
+let a = 5;
+let b = 10;
+
+console.log(a + b)
+
+//* using alert() to display a popup message to the user
+
+alert("alert()");
+
+console.error("This is an error message");
+console.warn("This is a warning message");
+console.debug("This is a debug message");
+console.info("This is an informational message");
+console.table({ name: "Alice", age: 30, city: "New York" });
+
