414cbdc6bea67950f8a823002b19db3539aef8a2
Snrbsk <sonerbasaak@gmail.com>
2025-11-12 11:31:23 +0300

    rearranging files and dialog box

---
+++ 1-basic-sctructures/5-dialog-boxes/app.js
@@ -0,0 +1,9 @@
+// dialog box: prompt, confirm, alert
+
+alert("This is an alert dialog box!");
+
+var name = prompt("This is a prompt dialog box! Please enter your name:", "Your name here");
+console.log("User name:", name);
+
+let isConfirmed = confirm("This is a confirm dialog box! Do you want to proceed?");
+console.log("User confirmed:", isConfirmed);
